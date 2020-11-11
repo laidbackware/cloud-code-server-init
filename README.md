3ad8a465c7b0105887775b6511bbacdc85b827f6
Ubuntu <ubuntu@jumpbox.c.pal4pe-1109-mproud.internal>
2020-11-11 20:57:32 +0000

    add gcp dns script

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # cloud-code-server-init
 Scripts to setup VSCode server with Lets Encrypt TLS on cloud instances
+
+# GCP
+- You must have set you instance to allow HTTPS traffic
+- Your instance must have an external IP
+- You must have 1 registered DNS zone
+- You must have jq installed
+## GCP Step
+1. SSH into the instance using the ubuntu user and clone this repo
+2. Login `gloud auth login`
+2. Run `dns-gcp.sh`
---
+++ dns-gcp.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+HOSTNAME=jumpbox
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Lookup first DNS zone, extract the name and remove trailing dot
+DNS_ZONE=$(gcloud dns managed-zones list --format json | \
+            jq .[0].dnsName |tr -d '"' )
+ZONE_NAME=$(gcloud dns managed-zones list --format json | \
+            jq .[0].name |tr -d '"' )
+EXTERNAL_IP=$(dig +short myip.opendns.com @resolver1.opendns.com)
+FQDN="${HOSTNAME}.${DNS_ZONE}"
+
+if nslookup $FQDN > /dev/null 2>&1
+then
+    echo "DNS record for ${FQDN} already exists. Exiting"
+    echo $FQDN | sed 's/.$//' > giot ${SCRIPT_DIR}/hostname
+    exit 1
+fi
+
+gcloud dns record-sets transaction start --zone=$ZONE_NAME
+
+gcloud dns record-sets transaction add "$EXTERNAL_IP"  --name="${FQDN}" \
+--ttl=300  --type=A --zone=$ZONE_NAME
+
+gcloud dns record-sets transaction execute --zone=$ZONE_NAME
+
+echo "\nCreated record: $FQDN = $EXTERNAL_IP"
+echo "Please wait a few minutes for it to propogate"
+
+echo $FQDN | sed 's/.$//' > ${SCRIPT_DIR}/hostname
